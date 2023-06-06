144b8bd3e2e0099bf4f352573b634ed9ae0aa8f4
Rusty Magnet <5367141+rustymagnet3000@users.noreply.github.com>
2023-06-06 13:50:55 +0100

    added first DDOS email notification (#8)

--- root.tf
+++ root.tf
@@ -4,3 +4,9 @@ module "access_rules" {
   cloudflare_token      = var.rm_cloudflare_token
   cloudflare_account_id = var.rm_cloudflare_account_id
 }
+
+module "notifications" {
+  source                     = "./modules/notifications"
+  cloudflare_account_id      = var.rm_cloudflare_account_id
+  su_email_for_notifications = var.rm_email_for_notifications
+}
--- variables.tf
+++ variables.tf
@@ -2,6 +2,14 @@ variable "domain" {
   default = "rustymagnet.xyz"
 }
 
+# env variable: TF_VAR_rm_email_for_notifications
+variable "rm_email_for_notifications" {
+  description = "Email of Cloudflare interested parties"
+
+  type    = string
+  default = ""
+}
+
 # env variable: TF_VAR_rm_cloudflare_account_id
 variable "rm_cloudflare_account_id" {
   description = "Cloudflare Account ID"
