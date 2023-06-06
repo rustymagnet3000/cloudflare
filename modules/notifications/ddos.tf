118e08f3179f7fcf38e4f013f69ee5e399e81375
rustymagnet3000 <5367141+rustymagnet3000@users.noreply.github.com>
2023-06-06 14:01:39 +0100

    added second notification

--- modules/notifications/ddos.tf
+++ modules/notifications/ddos.tf
@@ -1,11 +1,18 @@
 resource "cloudflare_notification_policy" "ddos_notifications_to_email" {
   account_id  = var.cloudflare_account_id
-  name        = "Layer 7 DDOS alerts"
-  description = "Notification policy to email Layer 7 DDOS summary"
+  name        = "${element(var.ddos_alerts, count.index)} notification"
+  description = "Notification policy for ${element(var.ddos_alerts, count.index)}"
   enabled     = true
-  alert_type  = "dos_attack_l7"
+  alert_type  = element(var.ddos_alerts, count.index)
+  count = length(var.ddos_alerts)
 
   email_integration {
     id = var.su_email_for_notifications
   }
+}
+
+
+variable "ddos_alerts" {
+  type    = list(string)
+  default = ["dos_attack_l7", "web_analytics_metrics_update"]
 }
