5f33596895f2fef4368c2716b7c4279410eb37c9
Rusty Magnet <5367141+rustymagnet3000@users.noreply.github.com>
2023-06-07 15:40:10 +0100

    count countries to challenge (#11)

--- modules/access_rules/country.tf
+++ modules/access_rules/country.tf
@@ -1,6 +1,6 @@
 resource "cloudflare_access_rule" "challenge_anzac" {
   account_id = var.cloudflare_account_id
-  notes      = "Challenge Australians and New Zealanders who like cricket :)"
+  notes      = "Challenge ${element(var.countries_naughty_list, count.index)}"
   mode       = "managed_challenge"
   count      = length(var.countries_naughty_list)
 
@@ -10,7 +10,4 @@ resource "cloudflare_access_rule" "challenge_anzac" {
   }
 }
 
-variable "countries_naughty_list" {
-  type    = list(string)
-  default = ["AU", "NZ"]
-}
+
