d902fee7f5697881cc007954d39553d5565b2742
Rusty Magnet <5367141+rustymagnet3000@users.noreply.github.com>
2023-06-07 15:34:02 +0100

    add more alerts (#10)

--- modules/notifications/ddos.tf
+++ modules/notifications/ddos.tf
@@ -1,10 +1,10 @@
-resource "cloudflare_notification_policy" "ddos_notifications_to_email" {
+resource "cloudflare_notification_policy" "notifications_to_email" {
   account_id  = var.cloudflare_account_id
-  name        = "${element(var.ddos_alerts, count.index)} notification"
-  description = "Notification policy for ${element(var.ddos_alerts, count.index)}"
+  name        = "${element(var.my_alerts, count.index)} notification"
+  description = "Notification policy for ${element(var.my_alerts, count.index)}"
   enabled     = true
-  alert_type  = element(var.ddos_alerts, count.index)
-  count = length(var.ddos_alerts)
+  alert_type  = element(var.my_alerts, count.index)
+  count       = length(var.my_alerts)
 
   email_integration {
     id = var.su_email_for_notifications
@@ -12,7 +12,13 @@ resource "cloudflare_notification_policy" "ddos_notifications_to_email" {
 }
 
 
-variable "ddos_alerts" {
-  type    = list(string)
-  default = ["dos_attack_l7", "web_analytics_metrics_update"]
-}
+variable "my_alerts" {
+  type = list(string)
+  default = [
+    "dos_attack_l7",
+    "fbm_dosd_attack",
+    "real_origin_monitoring",
+    "universal_ssl_event_type",
+    "expiring_service_token_alert",
+  ]
+}    
