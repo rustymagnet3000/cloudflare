75fe75b23bef45a0c19d09c29b32737694681733
Rusty Magnet <5367141+rustymagnet3000@users.noreply.github.com>
2023-06-20 14:30:26 +0100

    Tidy modules (#18)

---
+++ modules/firewall_rules/variables.tf
@@ -0,0 +1,24 @@
+variable "xyz_zone_id" {
+  description = "Zone ID passed from root Module"
+  type        = string
+  default     = ""
+}
+
+variable "cloudflare_account_id" {
+  description = "Cloudflare Account ID for Access Rules"
+
+  type    = string
+  default = ""
+}
+
+variable "countries_naughty_map" {
+  type    = map(any)
+  default = {}
+}
+
+variable "home_ip_address" {
+  description = "Home IP address"
+
+  type    = string
+  default = ""
+}
---
+++ modules/firewall_rules/versions.tf
@@ -0,0 +1,14 @@
+terraform {
+  required_providers {
+    cloudflare = {
+      source  = "cloudflare/cloudflare"
+      version = "= 4.7.1"
+    }
+  }
+  required_version = "~> 1.4.0"
+}
+
+
+
+
+
