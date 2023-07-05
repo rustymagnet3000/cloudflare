a7a3695765ee42e58d1be42b1346e5c56a106b65
Rusty Magnet <5367141+rustymagnet3000@users.noreply.github.com>
2023-07-05 11:23:04 +0100

    Firewall Filter with range of Bad IPs (#22)

---
+++ modules/firewall_rules/ip_group_block.tf
@@ -0,0 +1,14 @@
+resource "cloudflare_firewall_rule" "fw_challenge_ips_from_list" {
+  zone_id     = var.xyz_zone_id
+  description = "Requests coming from lists of bad IPs"
+  filter_id   = cloudflare_filter.filter_challenge_ips_from_list.id
+  action      = "managed_challenge"
+}
+
+
+resource "cloudflare_filter" "filter_challenge_ips_from_list" {
+  zone_id     = var.xyz_zone_id
+  description = "Requests coming from lists of bad IPs"
+  expression  = "(ip.src in { 192.168.0.100 192.168.0.101 })"
+}
+
