ac9e24d620e54b929b927974b4b9c1dccb7c3c89
rustymagnet3000 <5367141+rustymagnet3000@users.noreply.github.com>
2023-07-26 12:05:03 +0100

    cf ruleset added

---
+++ modules/rate_limits/basic_rate_limit.tf
@@ -0,0 +1,31 @@
+# not Advanced Rate Limit as no counting expression
+
+resource "cloudflare_ruleset" "zone_rl_custom_response" {
+  zone_id     = var.xyz_zone_id
+  name        = "Basic Rate Limit for my zone"
+  description = ""
+  kind        = "zone"
+  phase       = "http_ratelimit"
+
+  rules {
+    action = "block"
+    action_parameters {
+      response {
+        status_code  = 429
+        content      = "{\"response\": \"block\"}"
+        content_type = "application/json"
+      }
+    }
+    ratelimit {
+      characteristics     = ["ip.src", "cf.colo.id"]
+      period              = 10
+      requests_per_period = 5
+      requests_to_origin  = false
+      mitigation_timeout  = 10 # free plan has to use 10
+    }
+    # the following expression can't use HTTP Response Code, http.host or Request Method
+    expression  = "(http.request.uri.path contains \"/foobar\")"
+    description = "Rate limit requests to ${var.website}"
+    enabled     = true
+  }
+}
