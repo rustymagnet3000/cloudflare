d2d28548a1ba7ab477ec6dadc56ab1a14b75af96
rustymagnet3000 <5367141+rustymagnet3000@users.noreply.github.com>
2024-03-11 13:00:28 +0000

    march24

--- modules/firewall_rules/check_for_cookie.tf
+++ modules/firewall_rules/check_for_cookie.tf
@@ -9,13 +9,25 @@ resource "cloudflare_ruleset" "my_zone_custom_firewall" {
     action      = "managed_challenge"
     expression  = <<EOF
                 (
-                    http.request.uri.path contains "/"
+                    http.request.uri.path contains "/posts/"
                     and not any(lower(http.request.headers.names[*])[*]
                     contains "authorization")
                     and ip.geoip.country in { ${join(" ", var.my_markets)} }
                 )
     EOF
-    description = "Log requests that don't send auth credentials"
+    description = "Challenge requests that don't send authorization header from known country"
+    enabled     = true
+  }
+
+  rules {
+    action      = "block"
+    expression  = <<EOF
+                (
+                    http.request.uri.path contains "/"
+                    and (any(http.request.headers["foo"][*] == "bar"))
+                )
+    EOF
+    description = "Block requests that pass \"foo\" header with \"bar\" as the value"
     enabled     = true
   }
 }
