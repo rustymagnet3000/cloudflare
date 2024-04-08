d0d8676b73331f79287960d8fe386625aa4e77e8
Rusty Magnet <5367141+rustymagnet3000@users.noreply.github.com>
2024-04-08 09:10:33 +0100

    march24 (#32)

---
+++ modules/firewall_rules/rules.tf
@@ -0,0 +1,46 @@
+resource "cloudflare_ruleset" "my_zone_custom_firewall" {
+  zone_id     = var.xyz_zone_id
+  name        = "my_firewall_rules_inside_ruleset"
+  description = "Zone firewall rules"
+  kind        = "zone"
+  phase       = "http_request_firewall_custom"
+
+  rules {
+    action      = "managed_challenge"
+    expression  = <<EOF
+                (
+                    http.request.uri.path contains "/posts/"
+                    and not any(lower(http.request.headers.names[*])[*]
+                    contains "authorization")
+                    and ip.geoip.country in { ${join(" ", var.my_markets)} }
+                )
+    EOF
+    description = "Challenge requests that don't send authorization header from known country"
+    enabled     = true
+  }
+
+  rules {
+    action      = "block"
+    expression  = <<EOF
+                (
+                    http.request.uri.path contains "/"
+                    and (any(http.request.headers["foo"][*] == "bar"))
+
+                )
+    EOF
+    description = "Block requests that pass \"foo=bar\" Header"
+    enabled     = true
+  }
+
+  rules {
+    action      = "block"
+    expression  = <<EOF
+                (
+                    http.request.uri.path contains "/"
+                    and (any(lower(http.request.headers.names[*])[*] == "foo-id"))
+                )
+    EOF
+    description = "Block requests that pass \"foo-id\" Header value, regardless of value"
+    enabled     = true
+  }
+}
